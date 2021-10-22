b0d80c4f652e176fee2d4c9399a5677f5cf7b6bf
guoruipeng <guoruipeng@shuwen.com>
2021-10-22 17:59:09 +0800

    feat: 在 redux 中发起请求

---
+++ src/containers/Home/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header';
+import { connect } from 'react-redux';
+import { actions } from './store'
+
+class Home extends Component {
+  state = {}
+
+  // 只会在客户端渲染的时候被执行
+  componentDidMount() {
+    this.props.getHomeList()
+  }
+
+  getList = () => {
+    return this.props.newsList.map(item => <div key={item.id}>{item.title}</div>)
+  }
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <button onClick={() => console.log('HOME')}>打印</button>
+        {this.getList()}
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = (state) => ({
+  newsList: state.home.newsList,
+  name: state.home.name
+})
+
+const mapDispatchProps = dispatch => ({
+  getHomeList() {
+    dispatch(actions.getHomeList())
+  }
+})
+
+
+export default connect(mapStateToProps, mapDispatchProps)(Home)
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { reducer as homeReducer } from '../containers/Home/store'
 import thunk from 'redux-thunk'
 
-const reducer = (state = { name: 'Guo' }, actions) => {
-  return state
-}
+const reducer = combineReducers({
+  home: homeReducer
+})
 
 const getStore = () => {
   return createStore(reducer, applyMiddleware(thunk))
